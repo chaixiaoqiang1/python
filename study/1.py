87e7c491aff7685ffffbe8310d0704c72c324a09
Eagle <617701021@qq.com>
2017-09-29 16:16:46 +0800

    第一个python开始

--- study/1.py
+++ study/1.py
@@ -1,9 +1,21 @@
 # 刚开始写py,感觉还不错
 
+
+
+# 变量 运算 控制
 x = 100
 y = 50
 
 if x-y != 50:
 	print('不等于')
 else:
-	print('等于')	
+	print('等于')
+
+
+# 循环
+# 连接符是‘+’
+
+arr = ['a','b','c']
+
+for i in arr:
+	print('dsfdsfdsfkdasfkds'+'-----------'+i)
