93806fd0507b1469703f9111bcb7b55ce4e33085
Eagle <617701021@qq.com>
2017-09-29 21:50:23 +0800

    包下模块的使用

--- study/a.py
+++ study/a.py
@@ -1,3 +1,10 @@
-import b
+# import my_module
+#
+# my_module.func()
+# print(dir(my_module))
 
-b.func()
+
+# 包下面的模块引入方式  两种方式 一种是包下某个模块  一个是包下所有模块
+# from bao import mokuai
+from bao import *
+mokuai.test1()
