de94389a3c4098b1b3b91a2fc2c99a9d9b232f50
Eagle <617701021@qq.com>
2017-10-03 21:38:13 +0800

    随便写写

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-f = open('index.html','w')
-# data = f.read()
-f.write('过去的都是美好年华，留下的都是苟延残喘')
+# 爬虫的小试验 看来是成功了  好开心
+import urllib.request
 
+conn = urllib.request.urlopen("http://www.baidu.com")
+html_doc = conn.read().decode("utf-8")
+
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+print(soup.p.contents[0].string)
+print(soup.title.string) #获取标题内容
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+#
